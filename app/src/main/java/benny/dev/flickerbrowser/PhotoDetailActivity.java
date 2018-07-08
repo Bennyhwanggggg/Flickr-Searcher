815895c280301b1e5c42b5817653770b9e3f3ec6
Mitsunari <mitsunari@mitsunaris-mbp.home>
2018-07-08 22:58:29 +1000

    style modified and add style for prev android ver

--- app/src/main/java/benny/dev/flickerbrowser/PhotoDetailActivity.java
+++ app/src/main/java/benny/dev/flickerbrowser/PhotoDetailActivity.java
@@ -1,11 +1,16 @@
 package benny.dev.flickerbrowser;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class PhotoDetailActivity extends BaseActivity {
 
@@ -14,6 +19,23 @@ public class PhotoDetailActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_detail);
         activiateToolbar(true);
+
+        Intent intent = getIntent(); // retrieve the intent that started this activity.
+        Photo photo = (Photo) intent.getSerializableExtra(PHOTO_TRANSFER);
+        if(photo != null){
+
+            TextView photoTitle = (TextView) findViewById(R.id.photo_title);
+            photoTitle.setText("Title: " + photo.getTitle());
+
+            TextView photoTags = (TextView) findViewById(R.id.photo_tags);
+            photoTags.setText("Tags: " + photo.getTags());
+
+            TextView photoAuthor = (TextView) findViewById(R.id.photo_author);
+            photoAuthor.setText("Author: " + photo.getAuthor());
+
+            ImageView photoImage = (ImageView) findViewById(R.id.photo_image);
+            Picasso.get().load(photo.getLink()).error(R.drawable.placeholder).placeholder(R.drawable.placeholder).into(photoImage);
+        }
     }
 
 }
